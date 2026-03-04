41337b396e9043ec881fd250e746835a55b1f902
Hamza <financeworldinfo9596@gmail.com>
2026-03-04 16:26:52 +0500

    fix cors for vercel

--- server.js
+++ server.js
@@ -14,9 +14,19 @@ const app = express()
 await connectDB()
 
 // Middleware
-app.use(cors({
-  origin: '*'
-}));
+// ✅ Vercel ke liye sahi CORS claud
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+app.use(cors())
 
 app.use(express.json())
 
